b14853e6d429fd0e02430ec178f0db2d8c34f2ff
Hussein Omar <husseinomar6190@gmail.com>
2022-09-07 17:36:52 +0300

    testing the different buttons

--- App.tsx
+++ App.tsx
@@ -3,6 +3,7 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, Switch } from 'react-native';
 import { myColors } from './styles/Colors';
 import { ThemeContext } from './context/ThemeContext';
+import Button from './components/Button';
 
 export default function App() {
   const [theme, setTheme] = useState('light');
@@ -20,6 +21,9 @@ export default function App() {
           value={theme === 'light'}
           onValueChange={() => setTheme(theme === 'light' ? 'dark' : 'light')}
         />
+        <Button isGray title='3' onPress={() => alert('3')} />
+        <Button isBlue title='4' onPress={() => alert('4')} />
+        <Button title='5' onPress={() => alert('5')} />
         <StatusBar style={theme === 'light' ? 'dark' : 'light'} />
       </View>
     </ThemeContext.Provider>
