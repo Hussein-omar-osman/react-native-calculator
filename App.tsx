fea54558dd7c5f1d5d0bf824057ce64a606b6a9e
Hussein Omar <husseinomar6190@gmail.com>
2022-09-07 17:04:53 +0300

    adding theme switch

--- App.tsx
+++ App.tsx
@@ -1,11 +1,23 @@
+import { useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Switch } from 'react-native';
 
 export default function App() {
+  const [theme, setTheme] = useState('light');
   return (
-    <View style={styles.container}>
+    <View
+      style={
+        theme === 'light'
+          ? styles.container
+          : [styles.container, { backgroundColor: 'black' }]
+      }
+    >
       <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Switch
+        value={theme === 'light'}
+        onValueChange={() => setTheme(theme === 'light' ? 'dark' : 'light')}
+      />
+      <StatusBar style={theme === 'light' ? 'dark' : 'light'} />
     </View>
   );
 }
