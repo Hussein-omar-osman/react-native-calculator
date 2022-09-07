2f1a9c24bfb28a0607f31389af58d8cf557e1bc6
Hussein Omar <husseinomar6190@gmail.com>
2022-09-07 17:09:27 +0300

    creating a theme context provider

--- App.tsx
+++ App.tsx
@@ -1,31 +1,35 @@
 import { useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, Switch } from 'react-native';
+import { myColors } from './styles/Colors';
+import { ThemeContext } from './context/ThemeContext';
 
 export default function App() {
   const [theme, setTheme] = useState('light');
   return (
-    <View
-      style={
-        theme === 'light'
-          ? styles.container
-          : [styles.container, { backgroundColor: 'black' }]
-      }
-    >
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <Switch
-        value={theme === 'light'}
-        onValueChange={() => setTheme(theme === 'light' ? 'dark' : 'light')}
-      />
-      <StatusBar style={theme === 'light' ? 'dark' : 'light'} />
-    </View>
+    <ThemeContext.Provider value={theme}>
+      <View
+        style={
+          theme === 'light'
+            ? styles.container
+            : [styles.container, { backgroundColor: myColors.dark }]
+        }
+      >
+        <Text>Open up App.tsx to start working on your app!</Text>
+        <Switch
+          value={theme === 'light'}
+          onValueChange={() => setTheme(theme === 'light' ? 'dark' : 'light')}
+        />
+        <StatusBar style={theme === 'light' ? 'dark' : 'light'} />
+      </View>
+    </ThemeContext.Provider>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: myColors.light,
     alignItems: 'center',
     justifyContent: 'center',
   },
